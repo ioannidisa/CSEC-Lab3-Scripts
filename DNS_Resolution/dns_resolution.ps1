1442d9d0cd7f1bcae4091df5965f7f08d3c022fd
Sunggwan Choi <sxc2679@rit.edu>
2019-10-08 15:18:08 -0400

    Starting DNS_Resolution

---
+++ DNS_Resolution/dns_resolution.ps1
@@ -0,0 +1,8 @@
+<# 
+    Author: Sunggwan Choi 
+    Description: A script which does DNS resolution from the input file 
+    and figure out the corresponding IPv4 of the host 
+
+    Objective: A script for CSEC-465 Audit class in RIT 
+
+#>
