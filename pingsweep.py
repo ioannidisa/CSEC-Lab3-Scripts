c82f81aeed0549ab529a8159b1e307a750236159
LittleHack3r <sxm6549@rit.edu>
2019-10-08 21:44:57 -0400

    doneeee

--- pingsweep.py
+++ pingsweep.py
@@ -1,12 +1,66 @@
-# Ping Sweep
-# Audit, Lab 3
-# Shannon McHale
+import sys
+import os
+import ipaddress
+import struct
+import socket
+import argparse
 
-# take in command line arg
-# determine the IP range
+# get input
+parser = argparse.ArgumentParser(description='Input IP/Subnet or IP range')
+parser.add_argument('IPs', type=str, help='Input IP')
 
-# go through each IP
 
-# get hostname 
+def createint(strings):
+    x = 0
+    while x < len(strings):
+        strings[x] = int(strings[x])
+        x += 1
+    return strings
 
-# print hostnames
+
+def sort_ip(octets):
+    ip = ""
+    for octet in octets:
+        ip += str(octet) + "."
+    ip = ip[:-1]
+    return ip
+
+def main():
+    args = parser.parse_args()
+    useri = args.IPs
+    up = []
+
+    if '-' in useri:
+        start = useri.split('-')[0]
+        end = useri.split('-')[1]
+        start_octets_strings = start.split('.')
+        end_octets_strings = end.split('.')
+        start_octets = createint(start_octets_strings)
+        end_octets = createint(end_octets_strings)
+
+        ipstruct = struct.Struct('>I')
+        x, = ipstruct.unpack(socket.inet_aton(start))
+        y, = ipstruct.unpack(socket.inet_aton(end))
+        for i in range(x, y + 1):
+            response = os.system("ping -c 1 " + socket.inet_ntoa(ipstruct.pack(i)) + " > /dev/null")
+            if response == 0:
+                up.append(socket.inet_ntoa(ipstruct.pack(i)))
+
+    elif '/' in useri:
+        network = ipaddress.ip_network(useri)
+        for address in network:
+            response = os.system("ping -c 1 " + str(address) + " > /dev/null")
+            if response == 0:
+                up.append(str(address))
+    else:
+        print("Invalid formatting: " + useri)
+        sys.exit(1)
+
+        
+    print("Hosts that are up:")
+    for host in up:
+        print("\t" + host)
+    return up
+
+if __name__ == "__main__":
+    main()
